c6fcf9aeac0bc588891d1310524505205c8407fc
357157743 <37918761+357157743@users.noreply.github.com>
2020-06-04 17:52:49 +0800

    ...

--- src/main/java/com.mashibing.tank/cor/BulletTankCollider.java
+++ src/main/java/com.mashibing.tank/cor/BulletTankCollider.java
@@ -2,31 +2,33 @@ package com.mashibing.tank.cor;
 
 import com.mashibing.tank.*;
 
-/** 子弹 坦克相撞
+/**
+ * 子弹 坦克相撞
+ *
  * @date 2020/5/22 - 9:03
  */
 public class BulletTankCollider implements Collider {
     @Override
     public boolean collide(GameObject o1, GameObject o2) {
-        if(o1 instanceof Bullet && o2 instanceof Tank){
+        if (o1 instanceof Bullet && o2 instanceof Tank) {
             Bullet b = (Bullet) o1;
             Tank t = (Tank) o2;
-            if(b.group == t.getGroup()) return true;
+            if (b.group == t.getGroup()) return true;
 
-            if(b.rect.intersects(t.rect)){
+            if (b.rect.intersects(t.rect)) {
                 b.die();
                 t.die();
-                int eX = t.getX() + Tank.WIDTH/2 - Explode.WIDTH/2;
-                int eY = t.getY() + Tank.HEIGHT/2 - Explode.HEIGHT/2;
-                new Explode(eX,eY);
-                return false ;
+                int eX = t.getX() + Tank.WIDTH / 2 - Explode.WIDTH / 2;
+                int eY = t.getY() + Tank.HEIGHT / 2 - Explode.HEIGHT / 2;
+                new Explode(eX, eY);
+                return false;
             }
 
-            } else if( o2 instanceof Bullet && o1 instanceof Tank){
-                return collide(o2,o1);
+        } else if (o2 instanceof Bullet && o1 instanceof Tank) {
+            return collide(o2, o1);
 
-            }
-            return true;
+        }
+        return true;
 
     }
 
--- src/main/java/com.mashibing.tank/strategy/FourFireStrategy.java
+++ src/main/java/com.mashibing.tank/strategy/FourFireStrategy.java
@@ -12,14 +12,16 @@ public class FourFireStrategy implements FireStrategy {
    */
     @Override
     public void fire(Tank t) {
-        int bx = t.x + Tank.WIDTH/2- Bullet.WIDTH/2;
-        int by = t.y + Tank.HEIGHT/2-Bullet.HEIGHT/2;
+        int bx = t.x + Tank.WIDTH / 2 - Bullet.WIDTH / 2;
+        int by = t.y + Tank.HEIGHT / 2 - Bullet.HEIGHT / 2;
 
         Dir[] dirs = Dir.values();
-        for (Dir dir:dirs) {
-            new Bullet(bx,by,dir,t.group);
+        for (Dir dir : dirs) {
+            new Bullet(bx, by, dir, t.group);
         }
 
-        if(t.group == Group.GOOD) new Thread(()->{ new Audio("audio/tank_fire.wav").play();}).start();
+        if (t.group == Group.GOOD) new Thread(() -> {
+            new Audio("audio/tank_fire.wav").play();
+        }).start();
     }
 }
